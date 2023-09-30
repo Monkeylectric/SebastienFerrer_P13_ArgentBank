ad77cb9e4231c6f2596cf9844c87d2351318d327
Monkeylectric <78590742+Monkeylectric@users.noreply.github.com>
2023-09-30 10:28:29 +0200

    Add files via upload

---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import './SignIn.css';
+
+import Navbar from '../../components/Navbar/Navbar';
+import Main from '../../components/Main/Main';
+import Form from '../../components/Form/Form';
+import Footer from '../../components/Footer/Footer';
+
+// prop types
+
+function SignIn() {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <Main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <Form />
+                </section>
+            </Main>
+            <Footer />
+        </React.Fragment>
+    );
+}
+
+export default SignIn;
