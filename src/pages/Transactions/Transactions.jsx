d5c04643178e490010bb87c4db6b341f926c5658
Monkeylectric <78590742+Monkeylectric@users.noreply.github.com>
2023-09-30 10:28:44 +0200

    Add files via upload

---
+++ src/pages/Transactions/Transactions.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Transactions.css';
+
+import Navbar from '../../components/Navbar/Navbar';
+import Main from '../../components/Main/Main';
+
+import Footer from '../../components/Footer/Footer';
+
+function Transactions() {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <Main className="main bg-dark">
+                
+            </Main>
+            <Footer />
+        </React.Fragment>
+    );
+}
+
+export default Transactions;
